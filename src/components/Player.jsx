af6572d32e6602fc584e03d9b565d281e333a2b9
shefket-mustafa <miracle_1@abv.bg>
2025-04-27 22:06:56 +0300

    useRef practiced for user input and conditional display of the name if one available

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,19 @@
+import { useRef, useState } from "react";
 
 export default function Player() {
+
+  const [playerName, setPlayerName] = useState(null);
+  const player = useRef();
+
+  const nameHandler = () => {
+    setPlayerName(player.current.value);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome  {playerName ? playerName : 'unknown entity'} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={player} type="text" />
+        <button onClick={nameHandler}>Set Name</button>
       </p>
     </section>
   );
