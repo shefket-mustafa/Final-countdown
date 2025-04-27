50d6c99eda2d0d9d2ca016e816e7ee8d9feefb50
shefket-mustafa <miracle_1@abv.bg>
2025-04-27 22:45:11 +0300

    Challenge comp created

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,21 @@
+export default function TimerChallenge({ title, targetTime }) {
+  return (
+      
+    <>
+    <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+            {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+            <button>
+                Start Challenge
+            </button>
+        </p>
+        <p className="">
+            Time is Running ... / Timer inactive
+        </p>
+    </section>
+    </>
+    );
+}
