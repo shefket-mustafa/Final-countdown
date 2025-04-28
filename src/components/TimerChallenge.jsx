6fe47abb25f82ca0baf4370fd177c131b1561a16
shefket-mustafa <miracle_1@abv.bg>
2025-04-28 21:20:22 +0300

    Practiced using useRef
    practices using useRef for values that dont need to be in a state and dont have a big impact on the comp

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,19 +1,44 @@
+import { useRef, useState } from "react";
+
+// let timer;
+
 export default function TimerChallenge({ title, targetTime }) {
+
+    const timer = useRef();
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timeExpired, setTimeExpired] = useState(false);
+
+
+
+    const startHandle =  () => {
+        
+        timer.current = setTimeout(() => {
+            setTimeExpired(true);
+        }, targetTime * 1000);
+        
+        setTimerStarted(true);
+    };
+
+        const stopHandle = () => {
+            clearTimeout(timer.current);
+            setTimerStarted(false);
+        };
   return (
       
     <>
     <section className="challenge">
         <h2>{title}</h2>
+        {timeExpired && <p>You lost!</p>}
         <p className="challenge-time">
             {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
         <p>
-            <button>
-                Start Challenge
+            <button onClick={timerStarted ? stopHandle : startHandle}>
+                {timerStarted ? 'Stop' : 'Start'} Challenge
             </button>
         </p>
-        <p className="">
-            Time is Running ... / Timer inactive
+        <p className={timerStarted ? 'active' : undefined}>
+            {timerStarted ? 'Time is Running' : 'Timer inactive'} 
         </p>
     </section>
     </>
